72eede8f579bdc42a41eb035d1723452f9ec4cc2
Yoshimi Takuto <yocchantaku0127@gmail.com>
2023-09-09 08:57:47 +0900

    名前付きexport/import

--- index1.js
+++ index1.js
@@ -1,5 +1,7 @@
 // const naming = "ichiro";
 
 // モジュール間同士でもデータのやり取り
-import { naming } from './user.js';
-document.body.textContent = naming;
+import { naming, naming2, log } from './user.js';
+// 関数呼び出し
+log("foo");
+document.body.textContent = naming + naming2;
--- user.js
+++ user.js
@@ -1,2 +1,16 @@
 // モジュール間同士でもデータのやり取り
-export const naming = "takuto";
+const naming = "takuto";
+const naming2 = "kota";
+
+const log = (value) => {
+    console.log(value);
+};
+
+/*
+変数や関数を一つの「export」でまとめることもできる
+export { naming, naming2, log };
+*/ 
+
+// 以下のように「変数」「関数」で分けるとわかりやすくていいかも。。
+export { naming, naming2 }; //変数
+export { log }; //関数

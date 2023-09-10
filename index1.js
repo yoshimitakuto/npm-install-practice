e8010d930f5671c8e06de2f537179d808be0371a
Yoshimi Takuto <yocchantaku0127@gmail.com>
2023-09-10 10:05:25 +0900

    export default, import defaultの書き方

--- index1.js
+++ index1.js
@@ -1,5 +1,6 @@
 // モジュール間同士でもデータのやり取り
 import { naming as taku, naming2 as ko, log as logger } from './user.js';
+import naming, { log } from './user.js'; // default importの書き方（{}がいらない）
 import { naming as naru } from './user2.js'; //importでもエイリアスを使用でき、現場でよく採用されている
 // エイリアス「別名」を使用した時のimport文
 // import { taku, ko, logger } from './user.js';
@@ -7,6 +8,7 @@ import { naming as naru } from './user2.js'; //importでもエイリアスを使
 // 関数呼び出し
 // log(naming);
 logger(taku + naru); //エイリアス用
+log(naming);
 
 // document.body.textContent = naming + naming2;
 document.body.textContent = taku + ko + naru; //エイリアス用
--- user.js
+++ user.js
@@ -17,3 +17,21 @@ export { log }; //関数
 
 // エイリアス＝「別名」をexport/importで設定できる(asをつける)
 // export { naming as taku, naming2 as ko, log as logger };
+
+/*
+default exportを使用すると1モジュールにつき1回しか使用できない。
+また、エディタ上で補完が効きにくくなったり、名前がimportとexportで違うためリファクタリングがしにくくなるため、
+使用はあまり推奨されていない。
+しかし、Next.jsで一部使用される箇所があるため知っておくことで損はしない
+*/
+export default naming; // default exportの書き方
+// export default log; // このように1モジュールで2回以上使用することができない。
+
+/*
+また宣言した変数や関数には「export default const naming = "takuto"」のように使用できないが、
+以下のように、無名関数であれば使用ができる。
+export default (value) => {
+    console.log(value)
+};
+*/ 
+

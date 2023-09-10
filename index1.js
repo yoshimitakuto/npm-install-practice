a0a7464fc015c0d2daa6fd590782cc4e44a71e09
Yoshimi Takuto <yocchantaku0127@gmail.com>
2023-09-10 09:40:12 +0900

    開発現場で使われるのはimportでのエイリアスとなることが多い

--- index1.js
+++ index1.js
@@ -1,11 +1,12 @@
 // モジュール間同士でもデータのやり取り
-import { naming, naming2, log } from './user.js';
+import { naming as taku, naming2 as ko, log as logger, naming } from './user.js';
+import { naming as naru } from './user2.js'; //importでもエイリアスを使用でき、現場でよく採用されている
 // エイリアス「別名」を使用した時のimport文
 // import { taku, ko, logger } from './user.js';
 
 // 関数呼び出し
-log(naming);
-// logger(taku); //エイリアス用
+// log(naming);
+logger(taku + naru); //エイリアス用
 
-document.body.textContent = naming + naming2;
-// document.body.textContent = taku + ko; //エイリアス用
+// document.body.textContent = naming + naming2;
+document.body.textContent = taku + ko + naru; //エイリアス用
---
+++ user2.js
@@ -0,0 +1,3 @@
+const naming = "naru";
+
+export { naming };
